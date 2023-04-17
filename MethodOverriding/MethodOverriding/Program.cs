dd0e0f1b50a80716b9c7982c00a58312d5f49e92
kanhaiya-bhayana-inc <kanhaiya.bhayana@incedoinc.com>
2023-04-17 19:41:45 +0530

    Method Overriding :zap:

---
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MethodOverriding
+{
+    class Parent
+    {
+        public void Test()
+        {
+            Console.WriteLine("Called Test.Parent");
+        }
+        public virtual void Show()
+        {
+            Console.WriteLine("Called Show.Parent");
+        }
+    }
+    class Program : Parent
+    {
+        // Overloading Parent's Test method is Program class
+        public void Test(int i)
+        {
+            Console.WriteLine("Called Test.Program");
+        }
+
+        // Overriding Parent's Show method in Program class
+        public override void Show() // Overriding
+        {
+            Console.WriteLine("Called Show.Program");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Test();
+            p.Test(10);
+            p.Show();
+        }
+
+    }
+}
