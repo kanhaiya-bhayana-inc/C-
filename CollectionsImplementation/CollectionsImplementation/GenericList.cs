a42cf89061ecf7435be7f9dadf7368d5a63feccd
kanhaiya-bhayana-inc <kanhaiya.bhayana@incedoinc.com>
2023-04-18 18:34:46 +0530

    Generic List Implemented

---
+++ CollectionsImplementation/CollectionsImplementation/GenericList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+
+namespace CollectionsImplementation
+{
+    internal class GenericList
+    {
+        static void Main()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            for (int i = 0; i < list.Count; i++) { Console.Write(list[i] + " "); }
+            Console.WriteLine();
+
+            foreach(int i in list) Console.Write(i + " ");
+        }
+    }
+}
