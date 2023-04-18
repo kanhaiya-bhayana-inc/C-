80ee9cc3e2d52bd5f0251c7b82fbd72703d1e845
kanhaiya-bhayana-inc <kanhaiya.bhayana@incedoinc.com>
2023-04-18 18:01:46 +0530

    HashTable Added

---
+++ CollectionsImplementation/CollectionsImplementation/HashCollection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+
+namespace CollectionsImplementation
+{
+    internal class HashCollection
+    {
+        static void Main()
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("EmpID",1001);
+            ht.Add("EmpName", "Atul Mishra");
+            ht.Add("Phone", "+91-1234567890");
+            ht.Add("Address", "Delhi");
+
+            foreach (string key in ht.Keys)
+            {
+                Console.WriteLine(key + ": " + ht[key]);
+            }
+        }
+    }
+}
