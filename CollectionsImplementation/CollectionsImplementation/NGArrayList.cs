440c5f8ee8322d45cc1c09afd052b45299263328
kanhaiya-bhayana-inc <kanhaiya.bhayana@incedoinc.com>
2023-04-18 17:07:01 +0530

    Collections Initials :zap:

---
+++ CollectionsImplementation/CollectionsImplementation/NGArrayList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+
+namespace CollectionsImplementation
+{
+    internal class NGArrayList
+    {
+        static void Main()
+        {
+            ArrayList al = new ArrayList();
+
+            // Auto resizing
+            al.Add(100);al.Add(200);al.Add(300);al.Add(400);al.Add(500);
+            /*Console.WriteLine(al.Capacity);*/
+
+            // Insert at the mid
+            al.Insert(4,450);
+
+            // Remove specific value
+            //al.Remove(450); // It takes the value as a parameter that user want to delete...
+
+            // Remove form the position
+            al.RemoveAt(4); // It takes the index value as a parameter and value at that index will be deleted..
+
+            // For reversing the arrayList...
+            al.Reverse();
+
+            foreach (int i in al)
+                Console.Write(i + " ");
+        }
+        
+    }
+}
---
+++ CollectionsImplementation/CollectionsImplementation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionImplementation
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine();
+        }
+    }
+}
