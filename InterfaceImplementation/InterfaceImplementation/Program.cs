b73571155917e3a34657dfea96c5ae23d1b2a101
kanhaiya-bhayana-inc <kanhaiya.bhayana@incedoinc.com>
2023-04-18 12:57:28 +0530

    Interface initials :zap:

---
+++ InterfaceImplementation/InterfaceImplementation/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace InterfaceImplementation
+{
+    interface ItestInnterface1
+    {
+        void Add(int a,int b);
+    }
+    interface ItestInnterface2 : ItestInnterface1
+    {
+        void Sub(int a,int b);
+    }
+    class ImplementsInterface : ItestInnterface2
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine($"{a} + {b} = {a+b}");
+        }
+
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+
+        static void Main()
+        {
+            ImplementsInterface imp = new ImplementsInterface();
+            imp.Add(1,2);
+            imp.Sub(11,2);
+        }
+    }
+}
